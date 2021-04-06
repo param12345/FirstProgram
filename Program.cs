8f695f252c99b9fc402aa964e56ce553b0ebd1d7
param12345 <parampanjeta3@gmail.com>
2021-04-07 00:27:15 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, ");
+            Console.WriteLine("Welcome to c sharp....");
+        }
+    }
+}
